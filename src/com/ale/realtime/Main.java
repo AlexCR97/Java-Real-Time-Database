70c92fedbfc99cfe4d9b04219e5ec090cd3d3df2
AlexCR97 <carp_97@outlook.com>
2020-03-09 09:39:55 -0600

    Added methods for generic types

--- src/com/ale/realtime/Main.java
+++ src/com/ale/realtime/Main.java
@@ -4,12 +4,15 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
     
     public static void main(String[] args) {
         
-        Map<String, Object> map = new HashMap<>();
+        /*Map<String, Object> map = new HashMap<>();
         map.put("id", 1);
         map.put("email", "alex@live.com");
         map.put("password", "1234");
@@ -18,9 +21,9 @@ public class Main {
         JsonElement json = gson.toJsonTree(map);
         User user = gson.fromJson(json, User.class);
         
-        System.out.println(user);
+        System.out.println(user);*/
         
-        /*RealTimeDatabase db = new RealTimeDatabase(
+        RealTimeDatabase db = new RealTimeDatabase(
                 "localhost",
                 "3306",
                 "realtime_db",
@@ -28,30 +31,48 @@ public class Main {
                 "1234"
         );
         
-        db.startListening("users", 1000);
+        User user = new User();
+        user.setEmail("jayson@outlook.com");
+        user.setPassword("123456");
+        
+        try {
+            /*System.out.println("Adding user...");
+            db.add("users", user).get();*/
+        
+            System.out.println("Getting users...");
+            db.get("users", User.class).get().forEach(System.out::println);
+            
+            /*System.out.println("Getting user...");
+            System.out.println(db.get("users", "id", 5, User.class).get());*/
+        }
+        catch (InterruptedException | ExecutionException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        /*db.startListening("users", 1000);
         System.out.println("Listening table users...");*/
-        
+
         /*db.setOnChangeAllValuesListener("users", allValues -> {
-            System.out.println("Change in database! All values are:");
-            allValues.forEach(System.out::println);
+        System.out.println("Change in database! All values are:");
+        allValues.forEach(System.out::println);
         });*/
-        
+
         /*db.setOnChangeNewValuesListener("users", newValues -> {
-            System.out.println("Change in database! New values are:");
-            newValues.forEach(System.out::println);
+        System.out.println("Change in database! New values are:");
+        newValues.forEach(System.out::println);
         });*/
-        
+
         /*db.setOnChangeOldValuesListener("users", oldValues -> {
-            System.out.println("Change in database! Old values are:");
-            oldValues.forEach(System.out::println);
+        System.out.println("Change in database! Old values are:");
+        oldValues.forEach(System.out::println);
         });*/
-        
+
         /*try {
-            db.stopListening("users");
-            System.out.println("Stopped listening table users");
+        db.stopListening("users");
+        System.out.println("Stopped listening table users");
         }
         catch (Exception ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }*/
     }
 }
--- src/com/ale/realtime/RealTimeDatabase.java
+++ src/com/ale/realtime/RealTimeDatabase.java
@@ -1,5 +1,7 @@
 package com.ale.realtime;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -21,15 +23,15 @@ import java.util.stream.Collectors;
 public class RealTimeDatabase {
 
     public static interface OnChangeAllValuesListener {
-        public void onChangeAllValues(List<Map<String, Object>> values);
+        public void onChangeAllValues(List<Map<String, Object>> maps);
     }
     
     public static interface OnChangeNewValuesListener {
-        public void onChangeNewValues(List<Map<String, Object>> values);
+        public void onChangeNewValues(List<Map<String, Object>> maps);
     }
     
     public static interface OnChangeOldValuesListener {
-        public void onChangeOldValues(List<Map<String, Object>> values);
+        public void onChangeOldValues(List<Map<String, Object>> maps);
     }
     
     private final String host;
@@ -38,12 +40,13 @@ public class RealTimeDatabase {
     private final String user;
     private final String password;
     
+    private final Gson gson = new Gson();
     private final Map<String, ScheduledFuture> listeningThreads = new HashMap<>();
     private final Map<String, OnChangeAllValuesListener> allValuesListeners = new HashMap<>();
     private final Map<String, OnChangeNewValuesListener> newValuesListeners = new HashMap<>();
     private final Map<String, OnChangeOldValuesListener> oldValuesListeners = new HashMap<>();
     private final Map<String, List<Map<String, Object>>> oldValuesLists = new HashMap<>();
-
+    
     public RealTimeDatabase(String host, String port, String database, String user, String password) {
         this.host = host;
         this.port = port;
@@ -52,15 +55,15 @@ public class RealTimeDatabase {
         this.password = password;
     }
 
-    public CompletableFuture<Boolean> add(String table, Map<String, Object> values) {
+    public CompletableFuture<Boolean> add(String table, Map<String, Object> map) {
         return CompletableFuture.supplyAsync(() -> {
             try (Connection connection = getConnection().get()) {
-                List<String> columns = values.keySet().stream().collect(Collectors.toList());
-                String query = buildAddQuery(table, columns);
+                List<String> columnNames = map.keySet().stream().collect(Collectors.toList());
+                String query = buildAddQuery(table, columnNames);
                 PreparedStatement statement = connection.prepareStatement(query);
                 
                 int index = 1;
-                for (Map.Entry<String, Object> entry : values.entrySet()) {
+                for (Map.Entry<String, Object> entry : map.entrySet()) {
                     statement.setObject(index++, entry.getValue());
                 }
                 
@@ -72,6 +75,11 @@ public class RealTimeDatabase {
         });
     }
     
+    public <T extends Object> CompletableFuture<Boolean> add(String table, T object) {
+        Map<String, Object> map = pojoToMap(object);
+        return add(table, map);
+    }
+    
     public CompletableFuture<List<Map<String, Object>>> get(String table) {
         return CompletableFuture.supplyAsync(() -> {
             try (Connection connection = getConnection().get()) {
@@ -81,23 +89,41 @@ public class RealTimeDatabase {
                 ResultSet resultSet = statement.executeQuery();
                 ResultSetMetaData metaData = resultSet.getMetaData();
                 
-                List<Map<String, Object>> objects = new ArrayList<>();
+                List<Map<String, Object>> maps = new ArrayList<>();
                 
                 while (resultSet.next()) {
-                    Map<String, Object> object = new HashMap<>();
+                    Map<String, Object> map = new HashMap<>();
                 
                     for (int i = 1; i <= metaData.getColumnCount(); i++) {
                         String key = metaData.getColumnName(i);
                         Object value = resultSet.getObject(i);
-                        object.put(key, value);
+                        map.put(key, value);
                     }
                     
-                    objects.add(object);
+                    maps.add(map);
                 }
                 
-                return objects;
+                return maps;
             }
             catch (SQLException | InterruptedException | ExecutionException ex) {
+                throw new CompletionException(ex);
+            }
+        });
+    }
+    
+    public <T extends Object> CompletableFuture<List<T>> get(String table, Class<T> clazz) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                List<T> objects = new ArrayList<>();
+        
+                get(table).get().forEach(map -> {
+                    T object = mapToPojo(map, clazz);
+                    objects.add(object);
+                });
+
+                return objects;
+            }
+            catch (InterruptedException | ExecutionException ex) {
                 throw new CompletionException(ex);
             }
         });
@@ -112,19 +138,36 @@ public class RealTimeDatabase {
                 statement.setObject(1, idValue);
                 
                 ResultSet resultSet = statement.executeQuery();
+                
+                if (!resultSet.next())
+                    return null;
+                
                 ResultSetMetaData metaData = resultSet.getMetaData();
                 
-                Map<String, Object> object = new HashMap<>();
+                Map<String, Object> map = new HashMap<>();
                 
                 for (int i = 1; i <= metaData.getColumnCount(); i++) {
                     String key = metaData.getColumnName(i);
                     Object value = resultSet.getObject(i);
-                    object.put(key, value);
+                    map.put(key, value);
                 }
                 
-                return object;
+                return map;
             }
             catch (SQLException | InterruptedException | ExecutionException ex) {
+                throw new CompletionException(ex);
+            }
+        });
+    }
+    
+    public <T extends Object> CompletableFuture<T> get(String table, String idColumn, Object idValue, Class<T> clazz) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Map<String, Object> map = get(table, idColumn, idValue).get();
+                T object = mapToPojo(map, clazz);
+                return object;
+            }
+            catch (InterruptedException | ExecutionException ex) {
                 throw new CompletionException(ex);
             }
         });
@@ -146,15 +189,15 @@ public class RealTimeDatabase {
         });
     }
     
-    public CompletableFuture<Boolean> update(String table, Map<String, Object> values, String idColumn, Object idValue) {
+    public CompletableFuture<Boolean> update(String table, Map<String, Object> map, String idColumn, Object idValue) {
         return CompletableFuture.supplyAsync(() -> {
             try (Connection connection = getConnection().get()) {
-                List<String> columns = values.keySet().stream().collect(Collectors.toList());
-                String query = buildUpdateQuery(table, columns, idColumn);
+                List<String> columnNames = map.keySet().stream().collect(Collectors.toList());
+                String query = buildUpdateQuery(table, columnNames, idColumn);
                 PreparedStatement statement = connection.prepareStatement(query);
                 
                 int index = 1;
-                for (Map.Entry<String, Object> entry : values.entrySet()) {
+                for (Map.Entry<String, Object> entry : map.entrySet()) {
                     statement.setObject(index++, entry.getValue());
                 }
                 
@@ -168,17 +211,22 @@ public class RealTimeDatabase {
         });
     }
     
-    public String buildAddQuery(String table, List<String> columns) {
+    public <T extends Object> CompletableFuture<Boolean> update(String table, T object, String idColumn, Object idValue) {
+        Map<String, Object> map = pojoToMap(object);
+        return update(table, map, idColumn, idValue);
+    }
+    
+    public String buildAddQuery(String table, List<String> columnNames) {
         StringBuilder sb = new StringBuilder();
         
-        List<String> placeholders = columns.stream()
+        List<String> placeholders = columnNames.stream()
                 .map(column -> "?")
                 .collect(Collectors.toList());
         
         sb.append("INSERT INTO ")
                 .append(table)
                 .append('(')
-                .append(String.join(", ", columns))
+                .append(String.join(", ", columnNames))
                 .append(") VALUES (")
                 .append(String.join(", ", placeholders))
                 .append(')');
@@ -219,10 +267,10 @@ public class RealTimeDatabase {
         return sb.toString();
     }
     
-    public String buildUpdateQuery(String table, List<String> columns, String idColumn) {
+    public String buildUpdateQuery(String table, List<String> columnNames, String idColumn) {
         StringBuilder sb = new StringBuilder();
         
-        List<String> placeholders = columns.stream()
+        List<String> placeholders = columnNames.stream()
                 .map(column -> column + " = ?")
                 .collect(Collectors.toList());
         
@@ -318,5 +366,17 @@ public class RealTimeDatabase {
         }
         
         listeningThreads.get(table).cancel(true);
+    }
+    
+    private <T extends Object> T mapToPojo(Map<String, Object> map, Class<T> clazz) {
+        JsonElement json = gson.toJsonTree(map);
+        T object = gson.fromJson(json, clazz);
+        return object;
+    }
+    
+    private <T extends Object> Map<String, Object> pojoToMap(T object) {
+        String json = gson.toJson(object);
+        Map<String, Object> map = gson.fromJson(json, Map.class);
+        return map;
     }
 }
